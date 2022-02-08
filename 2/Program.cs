648f61d2595bd0b5b8cca0bec48610bc2c9be10a
Vogeel <vitorvogel12@gmail.com>
2022-02-08 15:47:59 -0300

    Add project files.

---
+++ 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine("ímpar: " + i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            for (int i = 1; i <= 500; i++) 
+            {
+                if (i % 2 != 0 && i % 3 == 0)
+                {
+                    total += i;
+
+                    
+                }
+            }
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+
+    }
+}
